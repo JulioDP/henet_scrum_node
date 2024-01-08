51c7071a6ce82c6a98a7c48df0d49e650550e759
JulioDP <chiapasjeremai@gmail.com>
2024-01-08 17:13:34 -0600

    Mi primer co
    clear

---
+++ init-firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDSUR-Ij-NwokD7jLLtudwAXYGEJePodoA",
+  authDomain: "ultra-ace-388716.firebaseapp.com",
+  projectId: "ultra-ace-388716",
+  storageBucket: "ultra-ace-388716.appspot.com",
+  messagingSenderId: "603797755097",
+  appId: "1:603797755097:web:58ab6591b4456926ca9ac8",
+  measurementId: "G-BMFM8WF4FQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
